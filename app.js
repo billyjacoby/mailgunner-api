2536a36062a5f39175d0e61949088834d5aecd3f
Billy Jacoby <billyjacoby@gmail.com>
2020-04-26 21:29:21 -0400

    added 200 status on option calls

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-});
-
 app.get("/", function (req, res) {
   res.send("Please send a post request");
 });
